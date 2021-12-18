511011d3328675d8a20d8e4dcabe3a8ad6676479
Rodney-glitch <rodneyovbiye@gmail.com>
2021-12-18 13:38:18 +0100

    Heroku

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '3+ywofacdsin#ea#&=c#t(ek_)r2bb+gd160dt#mqeu-%=opng'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
